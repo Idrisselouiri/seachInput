02cd3100172290535c0f16dafe21c6e41b50cd93
Idrisselouiri <contact.devlper@gmail.com>
2023-03-06 10:12:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let inputMessage = document.getElementById("input_message");
+
+function ShowValue(){
+    document.getElementById("valueMessage").innerHTML = inputMessage.value
+    inputMessage = "";
+}
